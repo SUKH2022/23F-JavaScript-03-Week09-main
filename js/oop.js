0d87b7cd1bac186073f79d4bbffcdd8ecc9ec172
Sukhpreet Saini <sukhpreet.saini2020@gmail.com>
2023-11-02 09:48:38 -0400

    lab3_week9

---
+++ js/oop.js
@@ -0,0 +1,123 @@
+const output = document.getElementById('output');
+const output2 = document.getElementById('output2');
+
+/* STEP 1a: Create a new object using a regular function */
+function createNewPerson(name) {
+    var obj = {};
+    obj.name = name;
+    obj.greetings = function () {
+        alert('Hello! I am ${this.name}');
+    };
+    return obj;
+}
+
+/* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
+var person1 = createNewPerson("Person1");
+var person2 = createNewPerson("Person2");
+
+/* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
+rewrite the above function, without returning anything. Capitalize the name of the function. */
+function Person(firstName, lastName, age, gender, interests) {
+    this.name = {
+        "firstName": firstName,
+        "lastName": lastName,
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function () {
+        return 'The interests of ${this.name.firstName} ${this.name.lastName} of age ${this.age} of gender ${this.gender} are ${this.interests}.';
+    }
+}
+
+/* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
+and again invoking the .greeting() method for each person */
+// var p1 = new Person("p1");
+var p2 = new Person("p2", "lName2", 20, "Male", ["dancing", "coding", "swimming"]);
+
+/* STEP 3a: Build the complete constructor for the object Person (comment out the above function first).
+ Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
+
+/* STEP 3b: Instantiate a new Person based on the above constructor */
+
+/* STEP 3c: Attempt to access the various properties of person1 using the console. */
+// person1['age']
+// person1.interests[1]
+// person1.bio()
+
+/* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
+var car = new Object();
+
+/* STEP 4b: Once 'car' is created, add various properties and methods… */
+car.brand = "Honda";
+car.color = "White";
+car.price = 50;
+
+/* STEP 4c: Change some of the properties of 'car' in the console, then invoke the car.fun() function */
+
+/* STEP 5a: Yet another approach is to use the create() method. 
+Let's see how the above car object can be used to create another object */
+var anotherCar = Object.create(car);
+
+/* STEP 5b: Output to the paragraph anotherCar.brand - you will see that it has retained the properties of the original object. */
+console.log(anotherCar.brand);
+
+// That's it! Now on to Lab 3...
+// Create a JavaScript function that permits the creation of a hamburger object
+// Consider that a hamburger has many options – that might include
+/*
+    different types of buns,
+    vegetable garnishes, 
+    assortments of cheeses, 
+    specialty sauces, 
+    different meat patties(beef, chicken, vegetarian), 
+    single, double, or triple patty, 
+    Pickles, hot peppers, olives etc.
+*/
+// add some extra if you want
+// Make sure that the function allows for the output of a sentence that describes the hamburger, too
+function createHamburger(options) {
+    const defaultOptions = {
+        buns: 'Regular',
+        garnishes: [],
+        cheeses: [],
+        sauces: [],
+        meat: 'Beef',
+        patties: 1,
+        toppings: [],
+    };
+
+    const hamburger = { 
+        defaultOptions: defaultOptions, 
+        options: options 
+    };
+
+    hamburger.describe = function () {
+        let description = "You have created a delicious hamburger with the following options:\n";
+        description += `-> Buns: ${hamburger.options.buns}\n`;
+        description += `-> Vegetable Garnishes: ${hamburger.options.garnishes.join(', ')}\n`;
+        description += `-> Cheeses: ${hamburger.options.cheeses.join(', ')}\n`;
+        description += `-> Specialty Sauces: ${hamburger.options.sauces.join(', ')}\n`;
+        description += `-> Meat: ${hamburger.options.meat}\n`;
+        description += `-> Patties: ${hamburger.options.patties}\n`;
+        description += `-> Toppings: ${hamburger.options.toppings.join(', ')}\n`;
+        return description;
+    };
+
+    return hamburger;
+}
+
+const myHamburger = createHamburger({
+    buns: 'Whole Wheat',
+    garnishes: ['Lettuce', 'Tomato', 'Onion', 'Pickles', 'Jalapeno'],
+    cheeses: ['Cheddar', 'Mozzarella'],
+    sauces: ['Ketchup', 'Mustard', 'Big Mac Sauce', 'Chicken Sauce', 'Thai Sauce', 'Chipotle Sauce'],
+    meat: 'Chicken',
+    patties: 2,
+    toppings: ['Olives', 'Avocado', 'Bacon', 'Mushrooms', 'Sliced Eggs'],
+});
+
+const descriptionElement = document.getElementById('output2');
+descriptionElement.innerHTML = myHamburger.describe().replace(/\n/g, '<br>');
+
+// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
